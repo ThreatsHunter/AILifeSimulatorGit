d8f714608579c691d08e3bd8207a0aa54fb447ee
Yan Costode <yan.costode@gmail.com>
2022-02-16 11:14:44 +0100

    IA Disappear On Position

--- Assets/Scripts/Entities/AI/ALS_AI.cs
+++ Assets/Scripts/Entities/AI/ALS_AI.cs
@@ -6,6 +6,7 @@ public class ALS_AI : MonoBehaviour
     [SerializeField] ALS_Home home = null;
     [SerializeField] NavMeshAgent navigation = null;
     [SerializeField] ALS_Build currentTarget = null;
+    Renderer render = null;
 
     public bool IsValid => home && navigation;
     public Vector3 TargetPosition => currentTarget ? currentTarget.transform.position : Vector3.zero;
@@ -14,6 +15,7 @@ public class ALS_AI : MonoBehaviour
     private void Start()
     {
         navigation = GetComponent<NavMeshAgent>();
+        render = GetComponent<Renderer>();
         World.Instance.OnHourChanged += SetCurrentTarget;
     }
     private void Update() => MoveToTarget();
@@ -32,16 +34,22 @@ public class ALS_AI : MonoBehaviour
         navigation.angularSpeed = World.Instance.WorldAcceleration * 120.0f;
         navigation.acceleration = World.Instance.WorldAcceleration * 8.0f;
         navigation.SetDestination(TargetPosition);
+
+        if (!navigation.hasPath && currentTarget.CanEnter())
+            SetActive(false);
+        else
+            SetActive(true);
     }
     void SetCurrentTarget(int _day, int _hour)
     {
         if (!IsValid) return;
         currentTarget = Planning[_day, _hour] ? Planning[_day, _hour] : (ALS_Build)home;
+        render.material.color = currentTarget.BuildColor;
     }
 
     public void SetActive(bool _status)
     {
-        GetComponent<Renderer>().enabled = _status;
-        GetComponent<NavMeshAgent>().enabled = _status;
+        render.enabled = _status;
+        navigation.obstacleAvoidanceType = _status ? ObstacleAvoidanceType.HighQualityObstacleAvoidance : ObstacleAvoidanceType.NoObstacleAvoidance;
     }
 }
--- Assets/Scripts/Entities/Build/ALS_Build.cs
+++ Assets/Scripts/Entities/Build/ALS_Build.cs
@@ -3,23 +3,9 @@ using UnityEngine;
 public abstract class ALS_Build : MonoBehaviour
 {
     [SerializeField] int amount = 0, capacity = 50;
+    [SerializeField] Color buildColor = Color.white;
 
-    public Color BuildColor { get; set; } = Color.white;
+    public Color BuildColor { get => buildColor; set => buildColor = value; }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        ALS_AI _ia = other.gameObject.GetComponent<ALS_AI>();
-        if (!_ia || !CanEnter()) return;
-        _ia.SetActive(false);
-        amount++;
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        ALS_AI _ia = other.gameObject.GetComponent<ALS_AI>();
-        if (!_ia) return;
-        _ia.SetActive(true);
-        amount--;
-    }
-
-    protected virtual bool CanEnter() => amount <= capacity;
+    public virtual bool CanEnter() => amount <= capacity;
 }
